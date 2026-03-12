3a213ca66a8dce2344869cec3fb16ad80de6b091
pablochumba <pablo.chumba79@gmail.com>
2026-03-11 21:20:22 -0300

    finished exercise 1 and 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        string letter = "A"; 
+
+        if (number >= 90)
+        {
+            Console.WriteLine($"You got an {letter}");
+        }        
+
+        else if (number >= 80)
+        {
+            letter = "B";
+            Console.WriteLine($"You got a {letter}");
+        }        
+
+        else if (number >= 70)
+        {
+            letter = "C";
+            Console.WriteLine($"You got a {letter}");
+        }        
+
+        else if (number >= 60)
+        {
+            letter = "D";
+            Console.WriteLine($"You got a {letter}");
+        }        
+
+        else if (number < 60)
+        {
+            letter = "F";
+            Console.WriteLine($"You got a {letter}");
+        }        
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course!");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
