9e0bd7b90f96436f6404bc0e1669fb00c7ee7dbd
pablochumba <pablo.chumba79@gmail.com>
2026-03-17 20:24:54 -0300

    finished week 02 learning activities

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+            job1._company = "Google";
+            job1._jobTitle = "Software Engineer";
+            job1._startYear = 2024;
+            job1._endYear = 2025;
+
+        Job job2 = new Job();
+            job2._company = "Microsoft";
+            job2._jobTitle = "Data Scientist";
+            job2._startYear = 2025;
+            job2._endYear = 2026;
+
+        Resume resume = new Resume();
+            resume._jobs.Add(job1);
+            resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
