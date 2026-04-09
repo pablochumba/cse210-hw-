019016bde818af183b748d8d9016944cc89d0a33
pablochumba <pablo.chumba79@gmail.com>
2026-04-09 19:03:44 -0400

    Finished learning activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Red", 4);
+        Rectangle rectangle = new Rectangle("Blue", 3, 5);
+        Circle circle = new Circle("Green", 2.5);
+
+        Console.WriteLine($"Square color: {square.GetColor()}, area: {square.GetArea()}");
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}, area: {rectangle.GetArea()}");
+        Console.WriteLine($"Circle color: {circle.GetColor()}, area: {circle.GetArea():F2}");
+
+        List<Shape> shapes = new List<Shape>
+        {
+            square,
+            rectangle,
+            circle,
+            new Square("Yellow", 6)
+        };
+
+        Console.WriteLine();
+        Console.WriteLine("Shapes in the list:");
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
